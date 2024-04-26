7f23c761525e25947d397d240c17ae850f8595f3
kawasaki0201 <22fi037@ms.dendai.ac.jp>
2024-04-26 11:06:35 +0900

    20240426-5

--- java-basic-2/src/XmasTreeKadai.java
+++ java-basic-2/src/XmasTreeKadai.java
@@ -1,3 +1,29 @@
 public class XmasTreeKadai {
-System.out.println("＊");
+    public static void main(String[] args) {
+        int height = 10; // ツリーの高さを設定します
+
+        // ツリーの各行を表示します
+        for (int i = 1; i <= height; i++) {
+            // スペースを追加して左揃えにします
+            for (int j = 1; j <= height - i; j++) {
+                System.out.print(" ");
+            }
+            // アスタリスクを追加します
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            // 改行します
+            System.out.println();
+        }
+
+        // 木の幹を表示します
+        for (int i = 1; i <= height - 1; i++) {
+            // スペースを追加します
+            for (int j = 1; j <= height - 1; j++) {
+                System.out.print(" ");
+            }
+            // 幹を追加します
+            System.out.println("*");
+        }
+    }
 }
