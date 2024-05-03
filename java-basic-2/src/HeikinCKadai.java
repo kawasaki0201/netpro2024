df0f0fd62da6cb66e62f2854ce0b59a8b376db02
kawasaki0201 <22fi037@ms.dendai.ac.jp>
2024-05-03 16:50:42 +0900

    20240503

--- java-basic-2/src/HeikinCKadai.java
+++ java-basic-2/src/HeikinCKadai.java
@@ -1,10 +1,58 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class HeikinCKadai {
-public static void main(String[] args) {
-    int h=0;
-    int[] hairetsu = new int[kamokus.length];
-    for(Kamoku k :kamokus){
-        hairetsu[h]=k.getScore();
-        h++;
+	public static final int N=100;
+	Kamoku[] kamoku = new Kamoku[N];
+	static String kamokuname="数学";
+
+	public static void main(String[] args) {
+		HeikinCKadai heikinb= new HeikinCKadai(kamokuname);
+		heikinb.initalizeScores();
+		heikinb.printAverage();
+		heikinb.gokakusha();
+
+	}
+	
+	HeikinCKadai(String s){
+		this.kamokuname=s;
+		
+	}
+	void initalizeScores(){
+		Random r = new Random();
+
+		for(int i=0;i<N;i++){
+			int score = r.nextInt(N+1);
+			kamoku[i]= new Kamoku(score);
+
+		}
+	
+	}
+	
+	void printAverage(){
+		int sum=0;
+		for(int i=0;i<N;i++){
+			sum+=kamoku[i].getScore();
+
+		}
+		System.out.println("*平均点は"+sum/N+"*");
+
+	}
+
+	void gokakusha(){
+        int h=0;
+        int[] hairetsu = new int[kamoku.length];
+        for(Kamoku k :kamoku){
+            hairetsu[h]=k.getScore();
+            h++;
+	}//student idと点数をソートしてだせ。＞＝８０以上
+Arrays.sort(hairetsu);
+System.out.println("*以下合格者の点数です。*");
+    for (int score : hairetsu) {
+        if (score >= 80) {
+            System.out.println("");
+            System.out.println("*" + score+"*");
+        }
     }
 }
 }
